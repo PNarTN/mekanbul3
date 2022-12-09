0678d847b38fbab839aef51b8348f74abc146154
PNarTN <pinartaylan07@gmail.com@google.com>
2022-12-09 17:14:38 +0300

    odev8 tamamlandi

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const axios = require("axios");
 var apiSecenekleri = {
-  sunucu: "https://mekanbul.pnarr.repl.co",
+  sunucu: "https://mekanbul3.pnarr.repl.co",
   apiYolu: "/api/mekanlar/",
 };
 var mesafeyiFormatla = function (mesafe) {
